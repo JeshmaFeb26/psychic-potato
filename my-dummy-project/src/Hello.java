5d2ee2eaae0fb9c7906098cf58c685f7e9d87f40
jeshma <jeshma.m26@gmail.com>
2021-03-23 17:42:48 +0530

    Change made in line 1

--- my-dummy-project/src/Hello.java
+++ my-dummy-project/src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 	
 	public void method() {
 		
-		System.out.println("hello");
+		System.out.println("hello J");
 		System.out.println("hi");
 		System.out.println("hi jeshma");
 		System.out.println("Hi developer");
